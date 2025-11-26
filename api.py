d7d191b01c04ef28da6e96218c5e38c33b7ba44e
knivesh <knivesh.stats@gmail.com>
2025-11-26 11:55:39 +0100

    Adding method to load model

--- api.py
+++ api.py
@@ -1,5 +1,10 @@
 import torch
 
+from utils import get_model_instance_segmentation
+
+NUM_CLASSES = 2
+CHECKPOINT_PATH=r"models\experiment1_epoch_4.pth"
+
 def get_device():
     """Dynamically determines the best device (CUDA/CPU) for the model."""
     if torch.accelerator.is_available():
@@ -8,5 +13,19 @@ def get_device():
         device = torch.device('cpu')
     return device
 
+def get_model():
+    """Loads the model once and caches it for all subsequent requests."""
+    device = get_device()
+
+    model = get_model_instance_segmentation(NUM_CLASSES)
+    checkpoint = torch.load(CHECKPOINT_PATH, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+
+    model.eval()
+    return model
+
 if __name__=="__main__":
-    print(get_device())
+    model = get_model()
+    print(f"Model state: {model.training=}")
+    print(f"Model device: {next(model.parameters()).device}")
